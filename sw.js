d49b78a277f988c1a62366bceef101a937fde0ba
Boris Seedorf <kontakt@privatpraxis-seedorf.de>
2026-05-18 02:11:28 +0200

    v2.1: JSON Backup Export + Import in Einstellungen; Cache v26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tinnitus-tracker-v25'; // APP_VERSION: v2.0
+const CACHE = 'tinnitus-tracker-v26'; // APP_VERSION: v2.1
 const FILES = [
   './TinnitusTracker_Seedorf.html',
   './manifest.json',
